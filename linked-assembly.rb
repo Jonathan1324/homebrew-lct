72d75224e2be6ad52135bf1a0405835f2988fba8
Jonathan1324 <mohrjonathan09@gmail.com>
2025-08-23 16:16:43 +0200

    fix

--- linked-assembly.rb
+++ linked-assembly.rb
@@ -4,30 +4,13 @@ class LinkedAssembly < Formula
   license "BSD-3-Clause"
   version "0.1.0-alpha.2"
 
-  stable do
-    if Hardware::CPU.arm?
-      url "https://github.com/Jonathan1324/linked-assembly/releases/download/v#{version}/linked-assembly-macos-arm64.tar.gz"
-      sha256 "c86ca5b4793c26296f81f5acb309b48ed0ea40c394928f5e92174440a66f143e"
-    else
-      url "https://github.com/Jonathan1324/linked-assembly/releases/download/v#{version}/linked-assembly-macos-x86_64.tar.gz"
-      sha256 "675a7b286d007cc3c3df714cde7d8a05eb2f0e0941c5c5f9e4466f6b3576dd66"
-    end
-
-#    if build.from_source?
-#      url "https://github.com/Jonathan1324/linked-assembly.git",
-#          tag: "v#{version}"
-#    end
+  if Hardware::CPU.arm?
+    url "https://github.com/Jonathan1324/linked-assembly/releases/download/v#{version}/linked-assembly-macos-arm64.tar.gz"
+    sha256 "c86ca5b4793c26296f81f5acb309b48ed0ea40c394928f5e92174440a66f143e"
+  else
+    url "https://github.com/Jonathan1324/linked-assembly/releases/download/v#{version}/linked-assembly-macos-x86_64.tar.gz"
+    sha256 "675a7b286d007cc3c3df714cde7d8a05eb2f0e0941c5c5f9e4466f6b3576dd66"
   end
-
-  head do
-    url "https://github.com/Jonathan1324/linked-assembly.git", branch: "main"
-  end
-
-#  depends_on "rust" => :build if build.from_source?
-#  depends_on "nasm" => :build if build.from_source?
-#  depends_on "python@3.12" => :build if build.from_source?
-#  depends_on "make" => :build if build.from_source?
-#  depends_on "gcc" => :build if build.from_source?
 
   def install
     system "tar", "-xzf", cached_download, "-C", prefix
